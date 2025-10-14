6a98e06d61b35314b176924fa3075461c8b6fede
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-10-14 10:07:17 +0100

    completing the about section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,9 +1,41 @@
+import { assets, infoList, toolsData } from "@/assets/assets"
+import Image from "next/image"
 
 const About = () => {
   return (
     <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
-        <h4>Introduction</h4>
-        <h2>About me</h2>
+        <h4 className="text-center text-lg mb-2">Introduction</h4>
+        <h2 className="text-center text-5xl">About me</h2>
+        
+        <div className="flex flex-col lg:flex-row w-full items-center gap-20 my-20">
+          <div className="w-64 sm:w-80 max-w-none rounded-3xl">
+            <Image src={assets.user_image} alt="user" className="w-full rounded-3xl" />
+          </div>
+            
+          <div className="flex-1">
+            <p className="mb-10 max-w-2xl">I am an experienced frontend developer with over a decade of professional expertise in the field. Throughout my career, I have had the privilege of collaborating with prestigious organizations, contributing to their success and growth</p>
+            
+            <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl">
+              {infoList.map(({icon, iconDark, title, description}, index) => (
+                <li key={index} className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500">
+                  <Image src={icon} alt={title} className="w-7 mt-3" />
+                  <h3 className="my-4 font-semibold text-gray-700">{title}</h3>
+                  <p className="text-gray-600 text-sm">{description}</p>
+                </li>
+              ))}
+            </ul>
+            
+            <h4 className="my-6 text-gray-700">Tools I use</h4>
+            
+            <ul className="flex items-center gap-3 sm:gap-6">
+              {toolsData.map((tool, index) => (
+                <li key={index} className="flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500">
+                  <Image src={tool} alt="tool" className="w-5 sm:w-7" />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
     </div>
   )
 }
