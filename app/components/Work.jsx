be9a70a26f5056a41701de0d7034550e82885dbb
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-10-15 23:53:31 +0100

    starting the work section

---
+++ app/components/Work.jsx
@@ -0,0 +1,13 @@
+
+const Work = () => {
+  return (
+    <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
+        <h4 className="text-center text-lg mb-2">My portfolio</h4>
+        <h2 className="text-center text-5xl">My latest work</h2>
+        
+        <p className="text-center max-w-2xl mx-auto mt-5 mb-12">Welcome to my web development portfolio! Explore a collection of projects showcasing my expertise in front-end development</p>
+    </div>
+  )
+}
+
+export default Work
