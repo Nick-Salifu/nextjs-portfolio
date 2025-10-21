f3226f7aeb5722574521443bb942a343d830eb6e
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-10-21 23:37:04 +0100

    added tailwindcss config file for better custom style application

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      color: {
+        lightHover: '#fcf4ff',
+        darkHover: '#2a004a',
+        darkTheme: '#11001f',
+      },
+      fontFamily: ["Outfit", "sans-serif"],
+      Ovo: ["Ovo", "serif"]
+    },
+  },
+  plugins: [],
+}
+
