c49cf8f38bad2a7fe6b3e24bbd748bec04c758cb
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-10-29 09:30:53 +0100

    updated the footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,5 +1,7 @@
 import { assets } from "@/assets/assets"
 import Image from "next/image"
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = ({darkMode}) => {
   return (
@@ -13,12 +15,18 @@ const Footer = ({darkMode}) => {
             </div>
         </div>
         
-        <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6">
+        <div className="w-full px-5 lg:px-8 xl:px-[8%] text-center sm:flex items-center justify-between border-t border-gray-400 mt-12 py-6">
             <p>&copy; 2025 Nick Salifu. All rights reserved.</p>
             <ul className="flex items-center justify-center gap-10 mt-4 sm:mt-0">
-                <li><a target="_blank" href="https://github.com/Nick-Salifu">GitHub</a></li>
-                <li><a target="_blank" href="https://linkedin.com/in/nicodemus-salifu">LinkedIn</a></li>
-                <li><a target="_blank" href="https://X.com/Captain_swenky">Twitter</a></li>
+                <li><a target="_blank" href="https://github.com/Nick-Salifu" className="flex items-center gap-2">
+                    <FaGithub size={20} /> GitHub</a>
+                </li>
+                <li><a target="_blank" href="https://linkedin.com/in/nicodemus-salifu" className="flex items-center gap-2">
+                    <FaLinkedin size={20} /> LinkedIn</a>
+                </li>
+                <li><a target="_blank" href="https://X.com/Captain_swenky" className="flex items-center gap-2">
+                    <FaXTwitter size={20} /> Twitter</a>
+                </li>
             </ul>
         </div>
     </div>
